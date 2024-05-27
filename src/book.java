1e0f2ae61b3b0de6ba9be54748e4c83292cc174b
ssalmanfaris <ssalmanfaris884@gmail.com>
2024-05-27 11:17:43 +0530

    library

---
+++ src/book.java
@@ -0,0 +1,9 @@
+public class book {
+    public static void main(String[] args) {
+
+        System.out.println("book name");
+        System.out.println("book id");
+        System.out.println("author");
+
+    }
+}
---
+++ src/library_system.java
@@ -0,0 +1,9 @@
+public class library_system {
+    public static void main(String[] args) {
+
+        System.out.println("books");
+        System.out.println("students");
+        System.out.println("library");
+
+    }
+}
